62c3e020ecf31f62929701fb92f9cfe4b4e56861
Elias Silveira <92744007+EliaSilveira@users.noreply.github.com>
2025-10-21 21:27:46 -0300

    Desafios

--- python/curso em video/Desafio/desafio34.py
+++ python/curso em video/Desafio/desafio34.py
@@ -4,4 +4,4 @@ if salario <= 1250:
     novosal = salario + (salario * 15 / 100)
 else:
     novosal = salario + (salario * 10 /100)
-print('Seu salario de R${:.2f} aumento para R${:.2f} agora.'.format(salario,novosal))
+print('Seu salario de \033[1;31mR${:.2f}\033[m aumento para \033[32mR${:.2f}\033[m agora.'.format(salario,novosal))
